17daae7b51547027895516b059f959862587361d
Sandman0083 <82945200+Sandman0083@users.noreply.github.com>
2021-11-25 19:41:29 +0530

    Add files via upload

---
+++ lgmtask3/sql/student_data.sql
@@ -0,0 +1,29 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+
+CREATE TABLE `student_data` (
+  `id` int(11) NOT NULL,
+  `u_name` text COLLATE utf8_unicode_ci NOT NULL,
+  `u_class` text COLLATE utf8_unicode_ci NOT NULL,
+  `u_rollno` text COLLATE utf8_unicode_ci NOT NULL,
+  `u_father` text COLLATE utf8_unicode_ci NOT NULL,
+  `u_mother` text COLLATE utf8_unicode_ci NOT NULL,
+  `u_mobile` text COLLATE utf8_unicode_ci NOT NULL,
+  `u_village` text COLLATE utf8_unicode_ci NOT NULL,
+  `u_image` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+ALTER TABLE `student_data`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `student_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
---
+++ lgmtask3/sql/user_marks.sql
@@ -0,0 +1,32 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+
+CREATE TABLE `user_mark` (
+  `id` int(11) NOT NULL,
+  `u_rollno` int(2) NOT NULL,
+  `u_class` int(2) NOT NULL,
+  `u_hindi1` int(2) NOT NULL,
+  `u_english1` int(2) NOT NULL,
+  `u_math1` int(2) NOT NULL,
+  `u_physics1` int(2) NOT NULL,
+  `u_chemestry1` int(2) NOT NULL,
+  `u_hindi2` int(2) NOT NULL,
+  `u_english2` int(2) NOT NULL,
+  `u_math2` int(2) NOT NULL,
+  `u_physics2` int(2) NOT NULL,
+  `u_chemestry` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+ALTER TABLE `user_mark`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `user_mark`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
